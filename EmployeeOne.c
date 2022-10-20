60e33b710011af96cb10449d9db5700632748649
jhhocs <joshuaho1@cpp.edu>
2022-10-19 19:40:11 -0700

    Added searchEmployeeByPhoneNumber and searchEmployeeBySalary functions

--- EmployeeOne.c
+++ EmployeeOne.c
@@ -22,3 +22,23 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
     } 
     return NULL; 
 } 
+
+PtrToEmployee searchEmployeeByPhoneNumber(PtrToConstEmployee ptr, int tableSize, char * targetPhoneNumber){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++) 
+    { 
+        if(strcmp(ptr->phone,targetPhoneNumber) == 0) 
+            return (PtrToEmployee) ptr; 
+    } 
+    return NULL; 
+} 
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++) 
+    { 
+        if(ptr->salary ==targetSalary == 0) 
+            return (PtrToEmployee) ptr; 
+    } 
+    return NULL; 
+} 
